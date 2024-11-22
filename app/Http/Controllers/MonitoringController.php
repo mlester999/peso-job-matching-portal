b6264ade829c61bf26ea94e5d1bfae91d995e9b0
mlester999 <mister6virgo@gmail.com>
2024-11-23 07:54:30 +0800

    updated monitoring search filter

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -139,12 +139,13 @@ class MonitoringController extends Controller
         ->with('applicant.user')
         ->when($searchReq, function($query, $search) {
             $query->where(function ($query) use ($search) {
-                $query->whereHas('user', function ($query) use ($search) {
+                $query->whereHas('applicant.user', function ($query) use ($search) {
                     $query->whereRaw('LOWER(email) LIKE LOWER(?)', ['%' . $search . '%'])
                         ->orWhereRaw('LOWER(contact_number) LIKE LOWER(?)', ['%' . $search . '%']);
-                })
-                        ->orWhereRaw('LOWER(first_name) LIKE LOWER(?)', ['%' . $search . '%'])
-                        ->orWhereRaw('LOWER(last_name) LIKE LOWER(?)', ['%' . $search . '%']);
+                })->orWhereHas('applicant', function ($query) use ($search) {
+                    $query->whereRaw('LOWER(first_name) LIKE LOWER(?)', ['%' . $search . '%'])
+                    ->orWhereRaw('LOWER(last_name) LIKE LOWER(?)', ['%' . $search . '%']);
+                });
             });
 
             // Use this if we like to perform a case-insensitive and approximate matching search
